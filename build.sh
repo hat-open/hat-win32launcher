9c39e6f3ff9c3605f5aee4cecb2ce1f00f389e52
bozo.kopic <bozo@kopic.xyz>
2023-04-26 16:06:38 +0200

    build update

--- build.sh
+++ build.sh
@@ -5,45 +5,45 @@ set -e
 CC=${CC:-x86_64-w64-mingw32-gcc}
 WINDRES=${WINDRES:-x86_64-w64-mingw32-windres}
 
-ROOT_PATH="$(cd $(dirname -- "$0"); pwd)"
-BUILD_PATH="$ROOT_PATH/build"
+ROOT_PATH="$(cd "$(dirname -- "$0")"; pwd)"
 
 NAME=win32_launcher
+BUILD_PATH=build
 ICON=
 ADMIN=
-while getopts n:i:a flag; do
+while getopts n:b:i:a flag; do
     case $flag in
         n)  NAME="$OPTARG";;
+        b)  BUILD_PATH="$OPTARG";;
         i)  ICON="$OPTARG";;
         a)  ADMIN=1;;
         ?)  ;;
     esac
 done
-shift $(($OPTIND - 1))
+shift $((OPTIND - 1))
 CMD="$*"
 
 if [ -z "$CMD" ]; then
-    printf "Usage: %s: [-n name] [-i icon] [-a] cmd\n" $0
+    printf "Usage: %s [-n name] [-b path] [-i icon] [-a] cmd\n" "$0"
     exit 1
 fi
 
-rm -rf $BUILD_PATH
-mkdir -p $BUILD_PATH
+mkdir -p "$BUILD_PATH"
 
-cat > $BUILD_PATH/main.rc << EOF
+cat > "$BUILD_PATH/main.rc" << EOF
 #include "winuser.h"
 STRINGTABLE { 1 "dummy" }
 EOF
 
-if [ ! -z "$ICON" ]; then
-ICON="$(cd $(dirname $ICON); pwd)/$(basename $ICON)"
-echo "iconId ICON \"$ICON\"" >> $BUILD_PATH/main.rc
+if [ -n "$ICON" ]; then
+ICON="$(cd "$(dirname "$ICON")"; pwd)/$(basename "$ICON")"
+echo "iconId ICON \"$ICON\"" >> "$BUILD_PATH/main.rc"
 fi
 
-if [ ! -z "$ADMIN" ]; then
+if [ -n "$ADMIN" ]; then
 MANIFEST="$BUILD_PATH/$NAME.exe.manifest"
-echo "1 RT_MANIFEST \"$MANIFEST\"" >> $BUILD_PATH/main.rc
-cat > $MANIFEST << EOF
+echo "1 RT_MANIFEST \"$MANIFEST\"" >> "$BUILD_PATH/main.rc"
+cat > "$MANIFEST" << EOF
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1"
           manifestVersion="1.0">
@@ -60,8 +60,8 @@ cat > $MANIFEST << EOF
 EOF
 fi
 
-$WINDRES $BUILD_PATH/main.rc $BUILD_PATH/main.rc.o
+$WINDRES "$BUILD_PATH/main.rc" "$BUILD_PATH/main.rc.o"
 $CC -O2 -mwindows \
     -o "$BUILD_PATH/$NAME.exe" \
     -DLAUNCHER_CMD="\"$CMD\"" \
-    $BUILD_PATH/main.rc.o main.c
+    "$BUILD_PATH/main.rc.o" "$ROOT_PATH/main.c"
